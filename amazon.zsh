8ed3973d441d50b8a94fdb605a76cb35e49e80ff
Tobias Becker <tobbecke@amazon.com>
2023-01-31 11:34:21 -0800

    Fix find Amazon mainline

--- amazon.zsh
+++ amazon.zsh
@@ -8,13 +8,18 @@ do
 	[[ -d $HOME/$append ]] && [[ :$PATH: == *:"$HOME/$append":* ]] || PATH="$PATH:$HOME/$append"
 done
 
-alias bb=brazil-build
-alias bba='brazil-build apollo-pkg'
-alias bre='brazil-runtime-exec'
-alias brc='brazil-recursive-cmd'
 alias bws='brazil ws'
 alias bwscreate='bws create -n'
-alias bbr='brc brazil-build'
-alias bball='brc --allPackages'
-alias bbb='brc --allPackages brazil-build'
-alias bbra='bbr apollo-pkg'
+
+bb() {brazil-build}
+alias bba='brazil-build apollo-pkg'
+alias bre='brazil-runtime-exec'
+if command -v brazil-recursive-cmd-parallel &>/dev/null; then
+    alias brc='brazil-recursive-cmd-parallel'
+else
+    alias brc='brazil-recursive-cmd'
+fi
+alias brb='brc brazil-build'
+alias bra='brc --allPackages'
+alias bbb='bra brazil-build'
+alias bbra='brb apollo-pkg'
