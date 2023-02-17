87be8dab1a8ae6ef1aa1e6d0cf6d069799916f63
Tobias Becker <tobbecke@amazon.com>
2023-02-16 16:41:08 -0800

    Add tunnel alias

--- amazon.zsh
+++ amazon.zsh
@@ -1,12 +1,12 @@
 # No duplicates!
 typeset -U path
 
-appends=(
+path_appends=(
     '.toolbox/bin'
     'Library/Application Support/Coursier/bin'
     'Library/Application Support/JetBrains/Toolbox/scripts'
 )
-for append in $appends;
+for append in $path_appends;
 do
 	[[ -d $HOME/$append ]] && path+="$HOME/$append"
 done
@@ -17,7 +17,8 @@ alias bwscreate='bws create -n'
 alias bb=brazil-build
 alias bba='brazil-build apollo-pkg'
 alias bre='brazil-runtime-exec'
-if command -v brazil-recursive-cmd-parallel &>/dev/null; then
+if command -v brazil-recursive-cmd-parallel &>/dev/null
+then
     alias brc='brazil-recursive-cmd-parallel'
 else
     alias brc='brazil-recursive-cmd'
@@ -29,6 +30,13 @@ alias bbra='brb apollo-pkg'
 alias -g D='-Ddebug.enable=y'
 alias -g DS='-Ddebug.enable=y -Ddebug.suspend=y'
 
+if command -v et >/dev/null
+then
+    alias tunnel='et -f -t 1044:1044,5005:5005 clouddesk'
+else
+    alias tunnel='ssh -A -L 1044:localhost:1044 -L 5005:localhost:5005 clouddesk'
+fi
+
 export BRAZIL_WORKSPACE_DEFAULT_LAYOUT=short
-export AUTO_TITLE_SCREENS='NO'
+export AUTO_TITLE_SCREENS=NO
 export AWS_EC2_METADATA_DISABLED=true
