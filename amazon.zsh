fb2d51228de0d3374e56939256271509db781c68
Tobias Becker <tobbecke@amazon.com>
2023-05-18 11:05:00 -0700

    Better bra and tunnel.

--- amazon.zsh
+++ amazon.zsh
@@ -24,18 +24,38 @@ else
     alias brc='brazil-recursive-cmd'
 fi
 alias brb='brc brazil-build'
-alias bra='brc --allPackages'
+bra() {
+    local cmd=$1
+    shift
+    local frame=$(printf '#%.0s' $(seq 1 30))
+    for dir in $(bws show --format json | jq -r '.packages[].source_location')
+    do
+        (
+            echo
+            echo $frame $(basename $dir) $frame
+            cd "$dir" || return
+            eval "$cmd" "$@"
+        )
+    done
+    echo
+}
 alias bbb='bra brazil-build'
 alias bbra='brb apollo-pkg'
 alias -g D='-Ddebug.enable=y'
 alias -g DS='-Ddebug.enable=y -Ddebug.suspend=y'
 
-if command -v et >/dev/null
-then
-    alias tunnel='et -f -t 1044:1044,5005:5005 clouddesk'
-else
-    alias tunnel='ssh -A -L 1044:localhost:1044 -L 5005:localhost:5005 clouddesk'
-fi
+tunnel() {
+    local host_name=clouddesk
+    [[ -S $SSH_AUTH_SOCK ]] || eval "$(ssh-agent)"
+    ssh-add -t 1d
+    if command -v et >/dev/null
+    then
+        local tunnel_cmd="et -f -t 1044:1044,5005:5005 $host_name"
+    else
+        local tunnel_cmd="ssh -A -L 1044:localhost:1044 -L 5005:localhost:5005 $host_name"
+    fi
+    scp {,$host_name:}~/.midway/cookie >/dev/null & eval "$tunnel_cmd"
+}
 
 if [[ `hostname` = *.ant.amazon.com ]]
 then
