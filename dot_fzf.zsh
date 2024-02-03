85e9aa57deb19b1e10292298cc3cd4c1f21a136e
Tobias Becker <tobbecke@amazon.com>
2024-02-02 16:16:54 -0800

    Remove unnecessary path append

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,9 +1,6 @@
 fzf_home="$(brew --prefix)/opt/fzf"
 [[ -d $fzf_home ]] || echo Not setting up fzf. && return
 
-typeset -U PATH path
-path+="$fzf_home/bin"
-
 if [[ -n $INTERACTIVE ]]; then
   source "$fzf_home/shell/completion.zsh" 2> /dev/null
   source "$fzf_home/shell/key-bindings.zsh"
