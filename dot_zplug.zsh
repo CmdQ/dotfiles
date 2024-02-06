cd11c00cf5550e0f1700002284680a99a2e85f1d
Tobias Becker <tobbecke@amazon.com>
2024-02-06 14:50:46 -0800

    Make better use of zplug.

--- dot_zplug.zsh
+++ dot_zplug.zsh
@@ -1,28 +1,23 @@
-zplug_home="$HOME/.zplug/init.zsh"
-
-#prompt_before="$PROMPT"
-
 # https://github.com/zplug/zplug
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 zplug_home="$HOME/.zplug/init.zsh"
 if [[ -f $zplug_home ]] && source "$zplug_home"
 then
-	zplug plugins/sudo, from:oh-my-zsh
-	zplug zsh-users/zsh-autosuggestions
+	zplug plugins/sudo, from:oh-my-zsh, lazy:true
+	zplug plugins/fzf, from:oh-my-zsh
+	zplug plugins/magic-enter, from:oh-my-zsh
+	zplug plugins/starship, from:oh-my-zsh, lazy:true
+	zplug zsh-users/zsh-autosuggestions, lazy:true
 
 	zplug b4b4r07/enhancd, use:init.sh
-	if zplug check b4b4r07/enhancd
-	then
-		export ENHANCD_COMMAND=cd
-		export ENHANCD_DOT_ARG='.'
-	fi
-
 	zplug plugins/macos, from:oh-my-zsh, if:'[ "$(uname)" = "Darwin" ]'
 
 	zplug plugins/ripgrep, from:oh-my-zsh, lazy:true
 	zplug plugins/fd, from:oh-my-zsh, lazy:true
 	zplug plugins/aws, from:oh-my-zsh, lazy:true
+	zplug plugins/git-commit, from:oh-my-zsh, lazy:true
+	zplug plugins/git-escape-magic, from:oh-my-zsh, lazy:true
 	if zplug check plugins/aws
 	then
 		export SHOW_AWS_PROMPT=false
@@ -41,8 +36,4 @@ then
 
 	# Then, source plugins and add commands to $PATH
 	zplug load
-
-	# Undo the prompt changes by the aws plugin.
-	#export PROMPT="$prompt_before"
-	#unset RPROMPT
 fi
