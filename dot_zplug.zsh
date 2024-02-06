c836cc7d5fa382576ad9aadb1331d8fd1c327c7b
Tobias Becker <tobbecke@amazon.com>
2024-02-05 17:29:39 -0800

    Optimize zplug.

--- dot_zplug.zsh
+++ dot_zplug.zsh
@@ -1,31 +1,39 @@
 zplug_home="$HOME/.zplug/init.zsh"
 
-prompt_before="$PROMPT"
+#prompt_before="$PROMPT"
 
 # https://github.com/zplug/zplug
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-if [[ -r $zplug_home ]] && source "$zplug_home"; then
-	zplug 'plugins/aws', from:oh-my-zsh
-	zplug 'plugins/fd', from:oh-my-zsh
-	zplug 'plugins/sudo', from:oh-my-zsh
+if [[ -r $zplug_home ]] && source "$zplug_home"
+then
+	zplug plugins/sudo, from:oh-my-zsh
+	zplug zsh-users/zsh-autosuggestions
 
-	zplug 'zsh-users/zsh-autosuggestions'
-
-	export ENHANCD_COMMAND=cd
-	export ENHANCD_DOT_ARG='.'
-	zplug 'b4b4r07/enhancd', use:init.sh
-
-	if [[ `uname` == Darwin ]]; then
-		zplug 'plugins/maxos', from:oh-my-zsh
+	zplug b4b4r07/enhancd, use:init.sh
+	if zplug check b4b4r07/enhancd
+	then
+		export ENHANCD_COMMAND=cd
+		export ENHANCD_DOT_ARG='.'
 	fi
 
-	zplug 'plugins/ripgrep', from:oh-my-zsh
+	zplug plugins/macos, from:oh-my-zsh, if:'[ "$(uname)" = "Darwin" ]'
+
+	zplug plugins/ripgrep, from:oh-my-zsh, lazy:true
+	zplug plugins/fd, from:oh-my-zsh, lazy:true
+	zplug plugins/aws, from:oh-my-zsh, lazy:true
+	if zplug check plugins/aws
+	then
+		export SHOW_AWS_PROMPT=false
+	fi
 
 	# Install plugins if there are plugins that have not been installed
-	if ! zplug check --verbose; then
+	if ! zplug check --verbose
+	then
 		printf 'Install? [y/N]: '
-		if read -q; then
-			echo; zplug install
+		if read -q
+		then
+			echo
+			zplug install
 		fi
 	fi
 
@@ -33,6 +41,6 @@ if [[ -r $zplug_home ]] && source "$zplug_home"; then
 	zplug load
 
 	# Undo the prompt changes by the aws plugin.
-	export PROMPT="$prompt_before"
-	unset RPROMPT
+	#export PROMPT="$prompt_before"
+	#unset RPROMPT
 fi
