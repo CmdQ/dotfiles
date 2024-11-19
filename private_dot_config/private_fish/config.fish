b9193f327c9a75836c50a5297bbf9a7979438dbd
Tobias Becker <nullpointeracception@gmail.com>
2024-11-19 11:58:50 -0800

    Add fish's variables.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.config/fish/aliases.fish
     source ~/.config/fish/git_aliases.fish
+
+    fish_add_path --global --prepend /opt/homebrew/bin $HOME/.local/bin $HOME/.dotnet/tools $HOME/.cargo/bin
 end
