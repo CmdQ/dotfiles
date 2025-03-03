67defb05e461bd34f8408aad8a626c8bbaed0dcb
Tobias Becker <nullpointeracception@gmail.com>
2025-03-03 09:58:56 -0800

    Fix homebrew path in fish.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,7 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.config/fish/aliases.fish
     source ~/.config/fish/git_aliases.fish
+end
 
-    fish_add_path --global --prepend /opt/homebrew/bin
-    fish_add_path --global $HOME/.local/bin $HOME/.dotnet/tools $HOME/.cargo/bin
+fish_add_path --global $HOME/.local/bin $HOME/.dotnet/tools $HOME/.cargo/bin
+
+set -l brew_paths /opt/homebrew /home/linuxbrew/.linuxbrew
+for path in $brew_paths
+    if test -x $path/bin/brew
+        eval ($path/bin/brew shellenv)
+        break
+    end
 end
