cb4073bf7e2cc446d26c532d6888b6b42a3800bc
Tobias Becker <tobiasbecker@microsoft.com>
2025-06-29 18:01:36 -0700

    Enable Fish completions.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -8,6 +8,8 @@ set -l brew_paths /opt/homebrew /home/linuxbrew/.linuxbrew
 for path in $brew_paths
     if test -x $path/bin/brew
         eval ($path/bin/brew shellenv)
+        # Add Homebrew completions to Fish's completion path
+        set -gx fish_complete_path $fish_complete_path $path/share/fish/vendor_completions.d
         break
     end
 end
